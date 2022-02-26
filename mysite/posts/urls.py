c5bbc3a903133f40ed34537f89dfcf56dc718d35
tim1609 <tim1609@inbox.ru>
2022-02-26 15:38:58 +0300

    Initial commit

---
+++ mysite/posts/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+
+app_name = 'posts'
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('group/', views.group_posts, name='group_posts'),
+
+]
