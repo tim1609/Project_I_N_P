b25338124d52c840d1b029a7f0761c95ff6a90d4
tim1609 <tim1609@inbox.ru>
2022-03-12 15:04:33 +0300

    site-page

--- mysite/posts/views.py
+++ mysite/posts/views.py
@@ -4,7 +4,57 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('Главная страница')
+    return HttpResponse(
+     '<!DOCTYPE html>'
+     ' <html>'
+     ' <head>'
+     ' <meta charset="utf-8">'
+     '<meta name="description" content="Краткое описание страницы">'
+     '<title>Тимофей</title>'
+     '<link rel="icon" href="fav.ico" type="image">'
+     '<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">'
+     '<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">'
+     '<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">'
+     '<link rel="manifest" href="/site.webmanifest">'
+     '<meta name="msapplication-TileColor" content="#da532c">'
+     '<meta name="theme-color" content="#ffffff">'
+     '</head>'
+     '<body>'
+     '<!-- Name -->'
+     '<h1> Тимофей </h1>'
+     '<!-- Surname -->'
+     '<h2> Сода </h2>'
+     '<!-- Idk -->'
+     '<h3> Иванович </h3>'
+     '<!-- Text -->'
+     '<p> Текст </p>'
+     '<p>  <i> Красивый текст </i>'
+     '<p>  <b> Жирный текст </b>  </p>'
+     '<!-- Cs -->'
+     '<p>  <a href=https://play-cs.com/ru/ >'
+     'CS '
+     '</a>  </p>'
+     '<!-- Image -->'
+     '<img width="800px" height="500px" src=http://2.bp.blogspot.com/-gSoC3dc2HpE/VeQKtTTh4XI/AAAAAAAAJsw/3qnbEwMWHzs/s1600/13756508429489.jpg >'
+     '<!-- Language -->'
+     '<p> Языки програмирования'
+     '<ul>'
+     '<li>Python</li>'
+     '<li>HTML</li>'
+     '<li>C++</li>'
+     '<li>JaVa</li>'
+     '</ul>  </p>'
+     '<!-- Firend -->'
+     '<p> Друзья'
+     '<ol> '
+     '<li>1</li>'
+     '<li>1</li>'
+     '<li>1</li>'
+     '<li>1</li>'
+     '</ol>  </p>'
+     '</body>'
+     '</html> ')
+
 
 
 
